a2b35d6d591a2b87f37e2f3753df35ea36e2338e
Jake Wretberg <dwretberg801@gmail.com>
2019-01-08 08:17:45 -0700

    added locations for product cards

--- page-objects/yoodlizePageObj.js
+++ page-objects/yoodlizePageObj.js
@@ -13,6 +13,127 @@ var yoodlizeCommands = {
           searchBtn: '[class="sc-esjQYD iIXhUv sc-ifAKCX kvYMhQ"]',
           browse: '[actionbuttonlarge="true"]',
 
+          //Tools category
+          seeAllTools: {
+            selector: '(//a[@href="/s?category=86"])[1]',
+            locateStrategy: 'xpath'
+          },
+          nextToolBtn: 'div:nth-child(1) > div:nth-child(2) > div > div.nextArrow.sc-kPVwWT.bUUAdh',
+          previousToolBtn: 'div:nth-child(1) > div:nth-child(2) > div > div.nextArrow.sc-kPVwWT.bOZYnu',
+          toolCardPickaxe: 'div[id="item-card-561"]', //Eagle Mountain Ut
+          toolCardWelder: 'div[id="item-card-44"]', //Kings County Ny
+          
+          //Toys category
+          seeAllToys: {
+            selector: '(//a[@href="/s?category=87"])[1]',
+            locateStrategy: 'xpath'
+          },
+          nextToysBtn: 'div:nth-child(1) > div:nth-child(5) > div > div.nextArrow.sc-kPVwWT.bUUAdh',
+          previousToysBtn: 'div:nth-child(1) > div:nth-child(5) > div > div.nextArrow.sc-kPVwWT.bOZYnu',
+          toysCardZorb: 'div[id="item-card-47"]', //Kings County Ny
+          toysCardTister: 'div[id="item-card-93"]', //Orem Ut
+
+
+          //Sporting Goods category
+          seeAllSports: {
+            selector: '(//a[@href="/s?category=76"])[1]',
+            locateStrategy: 'xpath'
+          },
+          nextSportsBtn: 'div:nth-child(1) > div:nth-child(8) > div > div.nextArrow.sc-kPVwWT.bUUAdh',
+          previousSportsBtn: 'div:nth-child(1) > div:nth-child(8) > div > div.nextArrow.sc-kPVwWT.bOZYnu',
+          sportsCardWiiU: 'div[id="item-card-615"]', //Flushing Ny
+          sportsCardSnakeGun: 'div[id="item-card-715"]', //Orem Ut
+
+          //Business Equipment category
+          seeAllBusiness: {
+            selector: '(//a[@href="/s?category=89"])[1]',
+            locateStrategy: 'xpath'
+          },
+          nextBusinessBtn: 'div:nth-child(5) > div:nth-child(2) > div > div.nextArrow.sc-kPVwWT.bUUAdh',
+          previousBusinessBtn: 'div:nth-child(5) > div:nth-child(2) > div > div.nextArrow.sc-kPVwWT.bOZYnu',
+          businessCardPhone: 'div[id="item-card-98"]', //Provo Ut
+          buisnessCardHitachi: 'div[id="item-card-54"]', //Kings County Ny
+
+          //Electronics category
+          seeAllElectronics: {
+            selector: '(//a[@href="/s?category=85"])[1]',
+            locateStrategy: 'xpath'
+          },
+          nextElectronicsBtn: 'div:nth-child(5) > div:nth-child(5) > div > div.nextArrow.sc-kPVwWT.bUUAdh',
+          previousElectronicsBtn: 'div:nth-child(5) > div:nth-child(5) > div > div.nextArrow.sc-kPVwWT.bOZYnu',
+          electronicsCardMacbookPro: 'div[id="item-card-39"]', //Kings County Ny
+          electronicsCardPs4: 'div[id="item-card-91"]', //Provo Ut
+
+          //Home, Lawn & Garden
+          seeAllHome: {
+              selector: '(//a[@href="/s?category=77"])[1]',
+              locateStrategy: 'xpath'
+          },
+          nextHomeBtn: 'div:nth-child(5) > div:nth-child(8) > div > div.nextArrow.sc-kPVwWT.bUUAdh',
+          previousHomeBtn: 'div:nth-child(5) > div:nth-child(8) > div > div.nextArrow.sc-kPVwWT.bOZYnu',
+          homeCardPoster: 'div[id="item-card-696"]', //Long Island City Ny
+          homeCardWacker: 'div[id="item-card-80"]', //Draper Ut
+
+          //Fashion
+          seeAllFashion: {
+              selector: '(//a[@href="/s?category=74"])[1]',
+              locateStrategy: 'xpath'
+          },
+          nextFashionBtn: 'div:nth-child(9) > div:nth-child(2) > div > div.nextArrow.sc-kPVwWT.bUUAdh',
+          previousFashionBtn: 'div:nth-child(9) > div:nth-child(2) > div > div.nextArrow.sc-kPVwWT.bOZYnu',
+          fashionCardWeddingDress: 'div[id="item-card-74"]', //Provo Ut
+          fashionCardDogpower: 'div[id="item-card-618"]', //Flushing Ny
+
+          //Outdoor Gear
+          seeAllGear: {
+            selector: '(//a[@href="/s?category=84"])[1]',
+            locateStrategy: 'xpath'
+        },
+          nextGearBtn: 'div:nth-child(9) > div:nth-child(5) > div > div.nextArrow.sc-kPVwWT.bUUAdh',
+          previousGearBtn: 'div:nth-child(9) > div:nth-child(5) > div > div.nextArrow.sc-kPVwWT.bOZYnu',
+          gearCardHammock: 'div[id="item-card-82"]', //Orem Ut
+          gearCardCampingSet: 'div[id="item-card-33"]', //Kings County Ny
+
+          //Navigators
+          seeAllNavigators: {
+            selector: '(//a[@href="/s?category=90"])[1]',
+            locateStrategy: 'xpath'
+        },
+          nextNavigatorsBtn: 'div:nth-child(9) > div:nth-child(8) > div > div.nextArrow.sc-kPVwWT.bUUAdh',
+          previousNavigatorsBtn: 'div:nth-child(9) > div:nth-child(8) > div > div.nextArrow.sc-kPVwWT.bOZYnu',
+          navigatorsCardSewing: 'div[id="item-card-102"]', //Spanish Fork Ut
+          navigatorsCardOldMan: 'div[id="item-card-187"]', //Elk Grove Ca
+
+          //Party
+          seeAllParty: {
+            selector: '(//a[@href="/s?category=88"])[1]',
+            locateStrategy: 'xpath'
+        },
+          nextPartyBtn: 'div:nth-child(13) > div:nth-child(2) > div > div.nextArrow.sc-kPVwWT.bUUAdh',
+          previousPartyBtn: 'div:nth-child(13) > div:nth-child(2) > div > div.nextArrow.sc-kPVwWT.bOZYnu',
+          partyCardMonkey: 'div[id="item-card-656"]', //Orem Ut
+          partyCardSnowCone: 'div[id="item-card-50"]', //Kings County Ny
+
+          //Venues
+          seeAllVenues: {
+            selector: '(//a[@href="/s?category=92"])[1]',
+            locateStrategy: 'xpath'
+        },
+          nextVenuesBtn: 'div:nth-child(13) > div:nth-child(5) > div > div.nextArrow.sc-kPVwWT.bUUAdh',
+          previousVenuesBtn: 'div:nth-child(13) > div:nth-child(5) > div > div.nextArrow.sc-kPVwWT.bOZYnu',
+          venuesCardHouse: 'div[id="item-card-107"]', //Provo Ut
+          venuesCardDoMyThing: 'div[id="item-card-617"]', //Flushing Ny
+          
+          //Recreational Vehicles
+          seeAllVehicles: {
+            selector: '(//a[@href="/s?category=91"])[1]',
+            locateStrategy: 'xpath'
+        },
+          nextVehiclesBtn: 'div:nth-child(13) > div:nth-child(8) > div > div.nextArrow.sc-kPVwWT.bUUAdh',
+          previousVehiclesBtn: 'div:nth-child(13) > div:nth-child(8) > div > div.nextArrow.sc-kPVwWT.bOZYnu'),
+          vehiclesCardBike: 'div[id="item-card-105"]', //Provo Ut
+          vehiclesCardHorse: 'div[id="item-card-706"]', //Anchorage Ak
+
           //Browse page
           filters: '[class="fal fa-chevron-circle-up fa-lg"]',
           toolsFilter: 'div:nth-child(6) > div.icheckbox_minimal-blue > ins',
@@ -28,115 +149,6 @@ var yoodlizeCommands = {
           maxPriceFilter: '[name="priceMax"]',
           clearFiltersBtn: '[class="sc-esjQYD kDoEhs sc-ifAKCX opXhf"]',
           applyFiltersBtn: '[class="sc-esjQYD bCvtjb sc-ifAKCX kvYMhQ"]',
-
-
-          //Tools category
-          seeAllTools: {
-            selector: '(//a[@href="/s?category=86"])[1]',
-            locateStrategy: 'xpath'
-          },
-          nextToolBtn: 'div:nth-child(1) > div:nth-child(2) > div > div.nextArrow.sc-kPVwWT.bUUAdh',
-          previousToolBtn: 'div:nth-child(1) > div:nth-child(2) > div > div.nextArrow.sc-kPVwWT.bOZYnu',
-          toolCardPickaxe: 'div[id="item-card-561"]',
-          
-          //Toys category
-          seeAllToys: {
-            selector: '(//a[@href="/s?category=87"])[1]',
-            locateStrategy: 'xpath'
-          },
-          nextToysBtn: 'div:nth-child(1) > div:nth-child(5) > div > div.nextArrow.sc-kPVwWT.bUUAdh',
-          previousToysBtn: 'div:nth-child(1) > div:nth-child(5) > div > div.nextArrow.sc-kPVwWT.bOZYnu',
-          toysCardZorb: 'div[id="item-card-47"]',
-
-          //Sporting Goods category
-          seeAllSports: {
-            selector: '(//a[@href="/s?category=76"])[1]',
-            locateStrategy: 'xpath'
-          },
-          nextSportsBtn: 'div:nth-child(1) > div:nth-child(8) > div > div.nextArrow.sc-kPVwWT.bUUAdh',
-          previousSportsBtn: 'div:nth-child(1) > div:nth-child(8) > div > div.nextArrow.sc-kPVwWT.bOZYnu',
-          sportsCardWiiU: 'div[id="item-card-615"]',
-
-          //Business Equipment category
-          seeAllBusiness: {
-            selector: '(//a[@href="/s?category=89"])[1]',
-            locateStrategy: 'xpath'
-          },
-          nextBusinessBtn: 'div:nth-child(5) > div:nth-child(2) > div > div.nextArrow.sc-kPVwWT.bUUAdh',
-          previousBusinessBtn: 'div:nth-child(5) > div:nth-child(2) > div > div.nextArrow.sc-kPVwWT.bOZYnu',
-          businessCardBooth: 'div[id="item-card-97"]',
-
-          //Electronics category
-          seeAllElectronics: {
-            selector: '(//a[@href="/s?category=85"])[1]',
-            locateStrategy: 'xpath'
-          },
-          nextElectronicsBtn: 'div:nth-child(5) > div:nth-child(5) > div > div.nextArrow.sc-kPVwWT.bUUAdh',
-          previousElectronicsBtn: 'div:nth-child(5) > div:nth-child(5) > div > div.nextArrow.sc-kPVwWT.bOZYnu',
-          electronicsCardMacbookPro: 'div[id="item-card-39"]',
-
-          //Home, Lawn & Garden
-          seeAllHome: {
-              selector: '(//a[@href="/s?category=77"])[1]',
-              locateStrategy: 'xpath'
-          },
-          nextHomeBtn: 'div:nth-child(5) > div:nth-child(8) > div > div.nextArrow.sc-kPVwWT.bUUAdh',
-          previousHomeBtn: 'div:nth-child(5) > div:nth-child(8) > div > div.nextArrow.sc-kPVwWT.bOZYnu',
-          homeCardPoster: 'div[id="item-card-696"]',
-
-          //Fashion
-          seeAllFashion: {
-              selector: '(//a[@href="/s?category=74"])[1]',
-              locateStrategy: 'xpath'
-          },
-          nextFashionBtn: 'div:nth-child(9) > div:nth-child(2) > div > div.nextArrow.sc-kPVwWT.bUUAdh',
-          previousFashionBtn: 'div:nth-child(9) > div:nth-child(2) > div > div.nextArrow.sc-kPVwWT.bOZYnu',
-          fashionCardDress: 'div[id="item-card-594"]',
-
-          //Outdoor Gear
-          seeAllGear: {
-            selector: '(//a[@href="/s?category=84"])[1]',
-            locateStrategy: 'xpath'
-        },
-          nextGearBtn: 'div:nth-child(9) > div:nth-child(5) > div > div.nextArrow.sc-kPVwWT.bUUAdh',
-          previousGearBtn: 'div:nth-child(9) > div:nth-child(5) > div > div.nextArrow.sc-kPVwWT.bOZYnu',
-          gearCardHammock: 'div[id="item-card-82"]',
-
-          //Navigators
-          seeAllNavigators: {
-            selector: '(//a[@href="/s?category=90"])[1]',
-            locateStrategy: 'xpath'
-        },
-          nextNavigatorsBtn: 'div:nth-child(9) > div:nth-child(8) > div > div.nextArrow.sc-kPVwWT.bUUAdh',
-          previousNavigatorsBtn: 'div:nth-child(9) > div:nth-child(8) > div > div.nextArrow.sc-kPVwWT.bOZYnu',
-          navigatorsCardSewing: 'div[id="item-card-102"]',
-
-          //Party
-          seeAllParty: {
-            selector: '(//a[@href="/s?category=88"])[1]',
-            locateStrategy: 'xpath'
-        },
-          nextPartyBtn: 'div:nth-child(13) > div:nth-child(2) > div > div.nextArrow.sc-kPVwWT.bUUAdh',
-          previousPartyBtn: 'div:nth-child(13) > div:nth-child(2) > div > div.nextArrow.sc-kPVwWT.bOZYnu',
-          partyCardMonkey: 'div[id="item-card-656"]',
-
-          //Venues
-          seeAllVenues: {
-            selector: '(//a[@href="/s?category=92"])[1]',
-            locateStrategy: 'xpath'
-        },
-          nextVenuesBtn: 'div:nth-child(13) > div:nth-child(5) > div > div.nextArrow.sc-kPVwWT.bUUAdh',
-          previousVenuesBtn: 'div:nth-child(13) > div:nth-child(5) > div > div.nextArrow.sc-kPVwWT.bOZYnu',
-          venuesCardHouse: 'div[id="item-card-107"]',
-          
-          //Recreational Vehicles
-          seeAllVehicles: {
-            selector: '(//a[@href="/s?category=91"])[1]',
-            locateStrategy: 'xpath'
-        },
-          nextVehiclesBtn: 'div:nth-child(13) > div:nth-child(8) > div > div.nextArrow.sc-kPVwWT.bUUAdh',
-          previousVehiclesBtn: 'div:nth-child(13) > div:nth-child(8) > div > div.nextArrow.sc-kPVwWT.bOZYnu'),
-          vehiclesCardBike: 'div[id="item-card-105"]',
       }
 
   }
