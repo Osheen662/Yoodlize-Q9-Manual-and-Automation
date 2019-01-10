8021417d6cb69c534ed89a1fd7967cd41a2c0390
Osheen662 <mroshinabraham@gmail.com>
2019-01-10 11:46:41 -0700

    Roshins page object updates

--- page-objects/yoodlizePageObj.js
+++ page-objects/yoodlizePageObj.js
@@ -24,8 +24,60 @@ var yoodlizeCommands = {
             locateStrategy: 'xpath'
           },
           toolCard: 'div[id="item-card-561"]',
-        
-        
-      }
+          detailsContainer: 'div#details-container',
+          description: {
+              selector:'//*[@id="details-container"]/div[1]/div[1]/div[1]/div/div[3]/div[2]/div[1]',
+              locateStrategy: 'xpath'
+          },
+          browseCategories: {
+              selector: '//*[text()="Browse Categories"]',
+              locateStrategy: 'xpath'
+          },
+          categoryTag: 'div[class="sc-iSDuPN haMjsI"]',
+          startDate: 'input[name="It is Start date id"]',
+          endDate: 'input[name= "It is End date id"]',
+          calender: 'div[aria-label="Calendar"]',
+          requestButton: {
+              selector: '//*[text()="Request"]',
+              locateStrategy: 'xpath',
+          },
+          emailInputBox: 'input[name="email"]',
+          passwordInputBox: 'input[name="password"]',
+          accountLogin: 'button[type="submit"]',
+          nextButton: {
+            selector: '//*[text()="Next"]',
+            locateStrategy: 'xpath',
+          },
+          aboutYourRental: {
+              selector: '//*[text()="About your rental"]',
+              locateStrategy: 'xpath',
+          },
+          rentalMessageInputBox: 'textarea[name="message"]',
+          ccNameInput: 'input[name="name"]',
+          ccCardNumberInput: 'input[name="cardNumber"]',
+          ccCVVInput: 'input[name="cvv"]',
+          ccExpiryInput: 'select[name="expiryDate"]',
+          ccYearInput: 'select[name="expiryYear"]',
+          payNowButton: {
+            selector: '//*[text()="Pay now"]',
+            locateStrategy: 'xpath',
+          },
+          reservationCode: {
+              selector: '//*[text()="Reservation code:"]',
+              locateStrategy: 'xpath',
+            },
+          viewReceipt: {
+                selector: '//*[text()="View Receipt"]',
+                locateStrategy: 'xpath',
+            },
+          viewListing: {
+            selector: '//*[text()="View Listing"]',
+            locateStrategy: 'xpath',
+            },
+          messageOwner: {
+              selector: '//*[text()="Message Owner"]',
+               locateStrategy: 'xpath',
+            },
 
   }
+}
