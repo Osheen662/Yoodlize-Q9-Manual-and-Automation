7ea851843fcace858f4a1fa88dd0a1fae116f8b5
Osheen662 <mroshinabraham@gmail.com>
2019-02-21 08:09:07 -0500

    Add files via upload

---
+++ nightwatch.props.js
@@ -0,0 +1,7 @@
+module.exports = {
+    resourcePath: "../testing-resources/",
+    seleniumServer: "selenium-server-standalone-3.141.59.jar",
+    chromedriver: "chromedriver.exe",//chrome
+    geckodriver: "geckodriver.exe",//firefox
+    edgedriver: "MicrosoftWebDriver.exe",//edge
+ }
